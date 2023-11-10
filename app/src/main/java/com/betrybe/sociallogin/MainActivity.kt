3080418fc5c4fd6fec32e4566206342ae5621538
tiagoaraujo00 <tiagodevjs@gmail.com>
2023-11-10 16:37:32 -0300

    linter

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -2,13 +2,8 @@ package com.betrybe.sociallogin
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
-
-    private val editTextEmail: TextInputEditText by lazy { findViewById(R.id.email_text_input_layout) }
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
