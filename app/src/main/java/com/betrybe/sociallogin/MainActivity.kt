3491ee6d25bd39834a3085c00436395c789a1f91
tiagoaraujo00 <tiagodevjs@gmail.com>
2023-11-13 09:50:43 -0300

    Req 06

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.betrybe.sociallogin
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    // private val email : TextView by lazy { findViewById(R.id.email_text_input_layout) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
