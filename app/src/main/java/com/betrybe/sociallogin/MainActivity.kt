30167ec5032df863dad2eab9562f1c1082707f03
tiagoaraujo00 <tiagodevjs@gmail.com>
2023-11-13 21:46:27 -0300

    Req 07 - erro

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -1,15 +1,38 @@
 package com.betrybe.sociallogin
 
 import android.os.Bundle
-import android.widget.TextView
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
-    // private val email : TextView by lazy { findViewById(R.id.email_text_input_layout) }
+    private val emailInput: TextInputLayout by lazy { findViewById(R.id.email_text_input_layout) }
+    private val passwordInput: TextInputLayout by lazy { findViewById(R.id.password_text_input_layout) }
+    private val button: Button by lazy { findViewById(R.id.login_button) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        emailInput.editText?.addTextChangedListener(loginTextWatcher)
+        passwordInput.editText?.addTextChangedListener(loginTextWatcher)
+    }
+
+    private val loginTextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            val email = emailInput.editText?.text.toString().trim()
+            val password = passwordInput.editText?.text.toString().trim()
+
+            button.isEnabled = email.isNotEmpty() && password.isNotEmpty()
+
+        }
+
+        override fun afterTextChanged(s: Editable?) {}
 
     }
 }
