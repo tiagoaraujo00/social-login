674553e7ac026d71b7ef833593d412893a98cbfa
tiagoaraujo00 <tiagodevjs@gmail.com>
2023-11-14 15:02:38 -0300

    Req 07 - erro lint corrigido

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.betrybe.sociallogin
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
 import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
@@ -16,20 +15,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        emailInput.editText?.addTextChangedListener(loginTextWatcher)
-        passwordInput.editText?.addTextChangedListener(loginTextWatcher)
+        emailInput.editText?.doOnTextChanged { _, _, _, _ -> loginTextWatcher() }
+        passwordInput.editText?.doOnTextChanged { _, _, _, _ -> loginTextWatcher() }
     }
 
-    private val loginTextWatcher = object : TextWatcher {
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            val email = emailInput.editText?.text.toString().trim()
-            val password = passwordInput.editText?.text.toString().trim()
-            button.isEnabled = email.isNotEmpty() && password.isNotEmpty()
-        }
-
-        override fun afterTextChanged(s: Editable?) {}
-
+    private fun loginTextWatcher() {
+        val email = emailInput.editText?.text.toString().trim()
+        val password = passwordInput.editText?.text.toString().trim()
+        button.isEnabled = email.isNotEmpty() && password.isNotEmpty()
     }
 }
