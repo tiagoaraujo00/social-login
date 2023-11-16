b154c2086d807196abc0262feb816fe8be4a9c2d
tiagoaraujo00 <tiagodevjs@gmail.com>
2023-11-16 11:04:51 -0300

    Req 08

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -17,11 +17,25 @@ class MainActivity : AppCompatActivity() {
 
         emailInput.editText?.doOnTextChanged { _, _, _, _ -> loginTextWatcher() }
         passwordInput.editText?.doOnTextChanged { _, _, _, _ -> loginTextWatcher() }
+        button.setOnClickListener {
+            val email = emailInput.editText?.text.toString().trim()
+            validateEmail(email, emailInput)
+        }
     }
 
     private fun loginTextWatcher() {
         val email = emailInput.editText?.text.toString().trim()
         val password = passwordInput.editText?.text.toString().trim()
         button.isEnabled = email.isNotEmpty() && password.isNotEmpty()
+    }
+
+    private fun validateEmail(email: String, editTextEmail: TextInputLayout) {
+        val emailPattern = Regex("[a-zA-Z\\d._-]+@[a-z]+\\.+[a-z]+")
+
+        if (!email.matches(emailPattern)) {
+            editTextEmail.error = "Email inválido"
+        } else {
+            editTextEmail.error = null
+        }
     }
 }
